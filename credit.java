6f7dd25ca3c292643b5a1442330c8b63d08e20fd
Payscout Inc <victor@payscout.com>
2017-11-08 16:43:16 +0200

    Update credit.java

--- credit.java
+++ credit.java
@@ -1,5 +1,5 @@
 HttpResponse<String> response = Unirest.post("https://gatewaystaging.payscout.com/api/process")
   .header("content-type", "application/x-www-form-urlencoded")
   .header("cache-control", "no-cache")
-  .body("client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123¤cy=USD&initial_amount=99.99")
+  .body("client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123&currency=USD&initial_amount=99.99")
   .asString(); 
